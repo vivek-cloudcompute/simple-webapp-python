9627e22865b5371e4c5e15c7334b4b2b2b79cd03
vivek-cloudcompute <156180005+vivek-cloudcompute@users.noreply.github.com>
2024-03-07 16:24:18 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-
-# Check if Docker is running
-if ! command -v docker &> /dev/null; then
-    echo "Docker is not installed or not running. Skipping Docker stop command."
-    exit 0
-fi
-
-# Check if any Docker containers are running with the specified ancestor
-if docker ps -q --filter ancestor=simple-web-python-app &> /dev/null; then
-    # Stop Docker containers with the specified ancestor
-    docker stop $(docker ps -q --filter ancestor=simple-web-python-app)
-    echo "Docker containers with ancestor 'simple-web-python-app' stopped."
-else
-    echo "No Docker containers with ancestor 'simple-web-python-app' are running. Skipping Docker stop command."
-fi
+echo "Hi"
