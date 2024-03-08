dc11391dd39219243945a8b7b92ca708fc1b93df
Vivekanandan V <vivek.mscse@gmail.com>
2024-03-08 16:43:03 +0530

    added23

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,5 +6,5 @@ set -e
 # Build Docker image
 sudo docker pull vivekcloud81/simple-webpython-project
 
-# Run Docker container
+# Run Docker container 
 sudo docker run -d -p 5000:5000 vivekcloud81/simple-webpython-project
